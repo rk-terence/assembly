c6e9dee07b99f79533b768472f2e2555149dcf75
rkterence <rkterence@zju.edu.cn>
2018-07-08 16:31:57 +0800

    2000T.asm Bug fix and improvment

--- 2000T.asm
+++ 2000T.asm
@@ -5,6 +5,15 @@ comment #
 is that it uses int8, and every 1/18 second, one more 'T' will be filled
 on the console, until the screen is full of 'T's, when you can press any
 key to terminate the program.
+
+==========BUG FIX===========
+There is a great bug in the first version. I changed the ISR for int8h, but
+I don't restore it. This caused disaster after I exited the program. In the
+new version, old int8h will be restored.
+
+Another modification is that I changed the rate of filling T. Now, every 1/18
+second, 10 'T's will be filled. This will be ten times faster, and you won't
+need to wait for a long time.
 #
 code segment
 assume cs:code
@@ -22,9 +31,11 @@ change_int8h:
     cli
     mov word ptr [bx], offset int8h
     mov [bx+2], cs
+    ;
     mov ax, 0B800h
     mov es, ax
-    mov cx, 2000
+    mov cx, 200
+    mov si, 10
     ;
     mov al, 'T'
     mov ah, 71h
@@ -33,15 +44,27 @@ change_int8h:
 wait_for_int8:
     cmp cx, 0
     jnz wait_for_int8
-    
+finally:
     ; getchar()
     mov ah, 01h
     int 21h
+    ; restore the point, to avoid crash of dos
+    cli
+    push word ptr old8h[0]
+    pop word ptr [bx]
+    push word ptr old8h[2]
+    pop word ptr [bx+2] 
+    sti
     ; exit
     mov ah, 4Ch
     int 21h
 int8h:
     stosw
+    dec si
+    cmp si, 0
+    jnz int8h
+    ;
+    mov si, 10
     dec cx
     jmp dword ptr old8h
 code ends
